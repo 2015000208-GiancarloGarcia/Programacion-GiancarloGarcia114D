87e8faff5f76fb0c11426c41ddca4596a8a58923
2015000208-GiancarloGarcia <g.garcia.5bdb@gmail.com>
2026-04-16 19:27:03 -0600

    Guia 2 Visual Notas

---
+++ Program (1).cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("RANGO DE ESTUDIANTES");
+        Console.WriteLine("Ingrese una letra()A, B, C, D, F");
+        char letra = Char.ToUpper(Console.ReadKey().KeyChar);
+        Console.WriteLine();
+
+        switch (letra) {
+            case 'A': 
+                Console.WriteLine("EXCELENTE");
+                break;
+            case 'B':
+                Console.WriteLine("BUENO");
+                break;
+            case 'C':
+                Console.WriteLine("REGULAR");
+                break;
+            case 'D':
+                Console.WriteLine("DEFICIENTE");
+                break;
+            case 'F':
+                Console.WriteLine("REPROBADO");
+                break;
+            default:
+                Console.WriteLine("Escriba una letra valida!!!");
+                break;
+        }
+    }
+}
